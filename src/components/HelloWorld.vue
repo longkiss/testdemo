537defc653f666670966c91e076e7be3d7164ac8
zxhuan <1123046380@qq.com>
2018-09-12 15:49:55 +0800

    w代码提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App,my name is zxhuan,hello world!!!!'
+      msg: 'Welcome to Your Vue.js App,my name is zxhuan,asdasdasasdhello world!!!!'
     }
   }
 }
